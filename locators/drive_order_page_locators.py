2875145d84d1d863a0a6d586669b97258b21cc8e
Mikhail Voronov <vbif542737@mail.ru>
2026-03-31 03:00:55 +0300

    Task_done

--- locators/drive_order_page_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class DriveOrderPageLocators:
-    DRIVE_PREVIEW = (By.CSS_SELECTOR, ".drive-preview")
-    RIGHTS_BUTTON = (By.CSS_SELECTOR, ".form .np-button")
-    RIGHTS_BUTTON_TEXT = (By.CSS_SELECTOR, ".form .np-button .np-text")
-    PAYMENT_BUTTON = (By.CSS_SELECTOR, ".form .pp-button")
-    REQUIREMENTS_HEADER = (By.CSS_SELECTOR, ".form .reqs .reqs-head")
